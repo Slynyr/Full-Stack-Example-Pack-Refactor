d55714e2b030cbbeedca539c6619aca48f148ea8
Sam Scott <samscott@mcmaster.ca>
2025-02-03 09:09:32 -0500

    commenting on hamburger.js

--- hamburger/js/hamburger.js
+++ hamburger/js/hamburger.js
@@ -1,17 +1,27 @@
-window.addEventListener("load", function() {
+/**
+ * adds a listener to open and close a menu
+ * Sam Scott, McMaster 2025
+ **/
+window.addEventListener("load", function(event) {
+
+  // Model: open = true when menu is open, false when closed
   let open = false;
+
+  // DOM Element Retrieval
   let menu = document.getElementById("hamburgercontent");
   let hamburger = document.getElementById("hamburger");
-  hamburger.addEventListener("click", function() {
+
+  /**
+   * Check "open" variable, show/hide menu as appropriate
+   */
+  hamburger.addEventListener("click", function(event) {
     if (open) {
       menu.style.display = "none";
       this.src = "images/hamburger.png";
-      this.style.padding = "0px";
       open = false;
     } else {
       menu.style.display = "flex";
       this.src = "images/hamburgerclose.png";
-      this.style.padding = "2px";
       open = true;
     }
   })
