1356aa0996dc006cd2edb84733ee4bc5a299040a
Sam Scott <samscott@mcmaster.ca>
2025-03-27 12:33:39 -0400

    minor change to hashing.php

--- hashing/hashing.php
+++ hashing/hashing.php
@@ -8,6 +8,7 @@
 //     normally you would get this from the DB where only the hashed 
 //     version is stored.
 $pwd = password_hash("mypassword", PASSWORD_BCRYPT);
+echo "hash of original password: '$pwd'<br>";
 
 // 2. get what the user entered
 $userpwd = filter_input(INPUT_POST, "password");
@@ -22,4 +23,4 @@ if (password_verify($userpwd, $pwd)) {
 // 4. Here's a new hash for the password you entered
 $newpwd = password_hash($userpwd, PASSWORD_BCRYPT);
 
-echo "<br>new hash '$newpwd'";
+echo "<br>hash of password received: '$newpwd'";
